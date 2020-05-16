a7e4d958e37b7dd30bcd0c9fd7b0ee01dba3ca09
Akihiko SHIRAI, Ph.D <shirai@mail.com>
2020-05-17 04:18:43 +0900

    readme更新

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # react-study
+
+## React入門
+
+
+1. `npm install -g create-react-app`: Facebook製のReactアプリのスケルトン
+1. `create-react-app test` : test プロジェクトの作成
+1. `cd test` : test ディレクトリに移動
+1. `npm start` : ブラウザ内でアプリが確認できる（http://localhost:3000）
+
+## 変数宣言
+
+（varではなく）const, letは ECMA Script2015 から使える変数宣言(通常ブラウザでは動作しないがbabelが翻訳してくれている)。
+const は代入不可能な変数、letは は代入可能な変数。コンパイルは通るがブラウザ内でエラーとして表示される。
+
--- test/src/index.js
+++ test/src/index.js
@@ -4,17 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// const, letは ECMA Script2015 から使える変数宣言(通常ブラウザでは動作しないがbabelが翻訳してくれている)
+// const は代入不可能な変数
+// letは は代入可能な変数
 const name = {
   firstName: 'Akihiko',
   lastName: 'SHIRAI'
+}
+let count = 0;
+// const count = 0; // もちろん const では以下の for文は動かない
+
+for (let i = 0 ; i < 10; i++) {
+  count ++;
 }
 
 function Hello(){
   return(
     <div>
-      <h1>こんにちわ { name.firstName + " "+  name.lastName } です ＝^_^＝</h1>
+      <h1>こんにちわ { name.firstName + " "+  name.lastName } です ＝^_^＝ { count + 1}  </h1>
       <p>Helloコンポーネントから書いています</p>
       <p>ReactJSから書くJSXには一つの要素しか含むことができない。</p>
+      <p>const, letはECMA Script2015から使える変数宣言</p>
     </div>
 
   );
