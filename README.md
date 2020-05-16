15ca90ac7476f23f5c8f888b4c07220fa7e800f2
Akihiko SHIRAI, Ph.D <shirai@mail.com>
2020-05-17 05:18:36 +0900

    HTMLとJSXの違い(予約名と例外)

--- README.md
+++ README.md
@@ -10,6 +10,33 @@
 
 ## 変数宣言
 
+ブレス``{}``を使って変数を表示することができる。
+
+
 （varではなく）const, letは ECMA Script2015 から使える変数宣言(通常ブラウザでは動作しないがbabelが翻訳してくれている)。
+
 const は代入不可能な変数、letは は代入可能な変数。コンパイルは通るがブラウザ内でエラーとして表示される。
+
+## HTMLとJSXの違い(予約名と例外)
+
+``<div className="ImageBox">`` というようにHTML/CSSのクラスは ``className`` と書く（JavaScriptのclassと被るため）と、生成されたHTMLでは``class``に置き換えられる。
+
+``label`` 要素の ``for`` 属性は ``htmlFor`` と書く（ ``for`` に変換される）
+
+## 独自属性
+
+``<div hoge="">`` といった属性を付けたいときは ``<div data-hoge="">``と書く。そうしないと無視されて生成されたHTML上では消滅する。
+JSXの変数名は基本はすべて ``lowerCamelCase`` 形式で書くが、 ``aria-*``, ``data-*`` のみ自由書式。
+
+## HTMLのエスケープ処理
+
+通常は文字列内のHTML要素はエスケープされる（＝クロスサイトスクリプティングなどを防ぐために、ltなどに変換される）。
+以下のように書くことで、エスケープをせずにHTMLとして扱うことができる。
+
+``someHtml = {__html: '<h2>これはH2要素</h2>' };``
+
+``dangerouslySetInnerHTML={someHtml} ``
+
+
+
 
--- test/src/index.js
+++ test/src/index.js
@@ -18,14 +18,31 @@ for (let i = 0 ; i < 10; i++) {
   count ++;
 }
 
+const someHtml = {__html: '<h2>これはH2要素</h2>' };
+
 function Hello(){
   return(
+    <section>
+
     <div>
       <h1>こんにちわ { name.firstName + " "+  name.lastName } です ＝^_^＝ { count + 1}  </h1>
-      <p>Helloコンポーネントから書いています</p>
       <p>ReactJSから書くJSXには一つの要素しか含むことができない。</p>
       <p>const, letはECMA Script2015から使える変数宣言</p>
+      <br />
+      <p id="hello">Helloコンポーネントから書いています</p>
     </div>
+  <span dangerouslySetInnerHTML={someHtml} />
+    <div style={{'margin-top': '100px', 'padding' : '50px', 'border' : 'dotted' }}>
+      <img src="https://goo.gl/0kZocc" alt="Google" />
+    </div>
+
+    {/*JSXのコメントはブレスで囲ってから書く
+      <div htmlFor="ImageBox">
+         <img src="https://goo.gl/0kZocc" alt="Google" />
+      </div>
+    */}
+  
+    </section>
 
   );
 }
